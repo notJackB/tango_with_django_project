dd85b9e02abf579f433031347236b3c1655cc762
Jack Brownlie <2576613B@student.gla.ac.uk>
2024-02-09 17:31:52 +0000

    exercise 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from rango.models import Category, Page
+#get our models from the rango app imported here
 
-# Register your models here.
+#create a class that can model the admin site itself
+#by importing the ModelAdmin module
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    
+
+#register them into the admin page
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+#formats the page site to have the new list system implemented
+#in the class above
+
+
+
+
--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+#category class with name as primary key
+#pass in the model from models to allow for database creation
+class Category(models.Model):
+    name= models.CharField(max_length=128, unique=True)
+    views= models.IntegerField(default=0)
+    likes= models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural= 'Categories'
+
+    #method to return the object's name
+    def __str__(self):
+        return self.name
+
+#page class that has the one to many realtionship to category
+class Page(models.Model):
+    category= models.ForeignKey(Category, on_delete=models.CASCADE)
+    #SQL to do make sure that if PK is deleted from category,
+    #remove associated FKs here
+    title= models.CharField(max_length=128)
+    url= models.URLField()
+    views= models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
